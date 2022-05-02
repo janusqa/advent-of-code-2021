9f8d1cf0562a4f03af82a4c2dc007b6da9ab027a
Janus Developer <dev@retrievo.net>
2022-05-02 09:24:52 -0400

    Refactor AoC 2021 Day 21 Part I

--- day21/day21.go
+++ day21/day21.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 )
 
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
 type Player struct {
 	position int
 	score    int
-}
-
-type Number interface {
-	int | int8 | int16 | int32 | int64 | float32 | float64
 }
 
 type DeterministicDice struct {
