656935927e238e1a41a5ace4f4df526cf8de8943
Janus Developer <dev@retrievo.net>
2022-05-02 09:23:33 -0400

    Refactor AoC 2021 Day 21 Part I

--- day21/day21.go
+++ day21/day21.go
@@ -13,10 +13,6 @@ type Number interface {
 	int | int8 | int16 | int32 | int64 | float32 | float64
 }
 
-type Dice interface {
-	roll(chances int) int
-}
-
 type DeterministicDice struct {
 	face   int
 	faces  int
